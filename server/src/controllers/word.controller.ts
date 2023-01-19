52c38d48109c0ef0d4bd6b2789c4cf74e8590c6c
SMARTOSC\lammx <lammx@smartosc.com>
2023-01-19 15:24:45 +0700

    abcd

---
+++ server/src/controllers/word.controller.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import Word from '../schemas/word.schema'
+
+async function Create(model: WordModel) : Promise<String>  {
+    var newWord = new Word();
+    newWord._id = new mongoose.Types.ObjectId()
+    newWord.word = model.word;
+    newWord.mean = model.mean;
+    await newWord.save()
+    return "Ok"
+}
+
+async function Update(_id: mongoose.Types.ObjectId, model: WordModel) : Promise<String> {
+    var update = await Word.findById(_id);
+    if (update) {
+        update.word = model.word;
+        update.mean = model.mean;
+        await update.update()
+        return "Ok"
+    }
+
+    return "item not found";
+}
+
+async function GetById(_id: mongoose.Types.ObjectId) : Promise<WordModel> {
+    var word = await Word.findById(_id);
+    
+}
+
+export { Create, Update }
